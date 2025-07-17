8b1b4d3fa11ab534d2a6ed5c4891543daa2a950d
Tausif Ahmed <tausif.ritu1@gmail.com>
2025-07-17 23:05:44 +0600

    fix the build issue encountered while accessing the user.stats

--- src/components/tables/user-tables.tsx
+++ src/components/tables/user-tables.tsx
@@ -105,12 +105,12 @@ export function UserTable({
                     </TableCell>
                     <TableCell className="px-2 md:px-4">
                       <span className="bg-yellow-100 text-yellow-800 px-2 py-1 rounded text-[10px] md:text-xs">
-                        {user.stats.coins ? user?.stats.coins : 0}
+                        {user?.stats ? user?.stats?.coins : 0}
                       </span>
                     </TableCell>
                     <TableCell className="px-2 md:px-4">
                       <span className="bg-yellow-100 text-yellow-800 px-2 py-1 rounded text-[10px] md:text-xs">
-                        {user.stats.diamonds ? user?.stats.diamonds : 0}
+                        {user?.stats ? user?.stats?.diamonds : 0}
                       </span>
                     </TableCell>
                     <TableCell className="px-2 md:px-4">
